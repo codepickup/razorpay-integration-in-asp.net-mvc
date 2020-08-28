4e17fef6f8d54ccb52c487e712c394a607691a79
Shoury <codepickup18@gmail.com>
2020-08-28 14:00:29 +0530

    Add project files.

---
+++ razorpaydemo/Controllers/PaymentController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace razorpaydemo.Controllers
+{
+    public class PaymentController : Controller
+    {
+        // GET: Payment
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult CreateOrder(Models.PaymentInitiateModel _requestData)
+        {
+            // Generate random receipt number for order
+            Random randomObj = new Random();
+            string transactionId = randomObj.Next(10000000, 100000000).ToString();
+
+            Razorpay.Api.RazorpayClient client = new Razorpay.Api.RazorpayClient("rzp_test_8wwPJmwEFiB3e6", "x6VIcdIETuW0iKNXNAcAYMLF");
+            Dictionary<string, object> options = new Dictionary<string, object>();
+            options.Add("amount", _requestData.amount * 100);  // Amount will in paise
+            options.Add("receipt", transactionId);
+            options.Add("currency", "INR");
+            options.Add("payment_capture", "0"); // 1 - automatic  , 0 - manual
+                                                 //options.Add("notes", "-- You can put any notes here --");
+            Razorpay.Api.Order orderResponse = client.Order.Create(options);
+            string orderId = orderResponse["id"].ToString();
+
+            // Create order model for return on view
+            OrderModel orderModel = new OrderModel
+            {
+                orderId = orderResponse.Attributes["id"],
+                razorpayKey = "rzp_test_8wwPJmwEFiB3e6",
+                amount = _requestData.amount * 100,
+                currency = "INR",
+                name = _requestData.name,
+                email = _requestData.email,
+                contactNumber = _requestData.contactNumber,
+                address = _requestData.address,
+                description = "Testing description"
+            };
+
+            // Return on PaymentPage with Order data
+            return View("PaymentPage", orderModel);
+        }
+
+        public class OrderModel
+        {
+            public string orderId { get; set; }
+            public string razorpayKey { get; set; }
+            public int amount { get; set; }
+            public string currency { get; set; }
+            public string name { get; set; }
+            public string email { get; set; }
+            public string contactNumber { get; set; }
+            public string address { get; set; }
+            public string description { get; set; }
+        }
+
+
+        [HttpPost]
+        public ActionResult Complete()
+        {
+            // Payment data comes in url so we have to get it from url
+
+            // This id is razorpay unique payment id which can be use to get the payment details from razorpay server
+            string paymentId = Request.Params["rzp_paymentid"];
+
+            // This is orderId
+            string orderId = Request.Params["rzp_orderid"];
+
+            Razorpay.Api.RazorpayClient client = new Razorpay.Api.RazorpayClient("rzp_test_8wwPJmwEFiB3e6", "x6VIcdIETuW0iKNXNAcAYMLF");
+
+            Razorpay.Api.Payment payment = client.Payment.Fetch(paymentId);
+
+            // This code is for capture the payment 
+            Dictionary<string, object> options = new Dictionary<string, object>();
+            options.Add("amount", payment.Attributes["amount"]);
+            Razorpay.Api.Payment paymentCaptured = payment.Capture(options);
+            string amt = paymentCaptured.Attributes["amount"];
+
+            //// Check payment made successfully
+
+            if (paymentCaptured.Attributes["status"] == "captured")
+            {
+                // Create these action method
+                return RedirectToAction("Success");
+            }
+            else
+            {
+                return RedirectToAction("Failed");
+            }
+        }
+
+        public ActionResult Success()
+        {
+            return View();
+        }
+
+        public ActionResult Failed()
+        {
+            return View();
+        }
+    }
+}
---
+++ razorpaydemo/Models/PaymentInitiateModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace razorpaydemo.Models
+{
+    public class PaymentInitiateModel
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string contactNumber { get; set; }
+        public string address { get; set; }
+        public int amount { get; set; }
+    }
+}
